4e68468a04ea1ed260a8b538b3ea2aac61858376
gshelly <gshelly1402@gmail.com>
2021-07-11 16:01:44 -0700

    Created Person componenct using React js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Person from './components/Person';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person firstName="Shelly" lastName="Gupta" age="30" hairColor="Brown" />
+      <Person firstName="Jatin" lastName="Gupta" age="30" hairColor="Black" />
+      <Person firstName="John" lastName="deo" age="35" hairColor="Red" />
     </div>
   );
 }
