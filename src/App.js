2401a5c568d592a9d32b5a34b1f34340e7518c39
gshelly <gshelly1402@gmail.com>
2021-07-11 23:31:38 -0700

    created a birthday button that will increase one person's age by one

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Person firstName="Shelly" lastName="Gupta" age="30" hairColor="Brown" />
-      <Person firstName="Jatin" lastName="Gupta" age="30" hairColor="Black" />
-      <Person firstName="John" lastName="deo" age="35" hairColor="Red" />
+      <Person firstName="Shelly" lastName="Gupta" age={ 20 } hairColor="Brown" />
+      <Person firstName="Jatin" lastName="Gupta" age={ 30 } hairColor="Black" />
+      <Person firstName="John" lastName="deo" age={ 35 } hairColor="Red" />
     </div>
   );
 }
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Person = (props) => {
+    const {firstName, lastName, age, hairColor} = props
+    const [getAge, setAge] = useState(age)
     return(
         <div>
-            <h1>{props.lastName}, {props.firstName}</h1>
-            <p>Age: {props.age}</p>
-            <p>Hair Color: {props.hairColor}</p>
+            <h1>{lastName}, {firstName}</h1>
+            <p>Age: {getAge}</p>
+            <p>Hair Color: {hairColor}</p>
+            <button onClick = {(event) => setAge( getAge + 1) }> Birthday Button for {firstName} {lastName}</button>
         </div>
     )
 }
