931bfc3e15ccef81beeedbdc34108ebe32cebf7a
gshelly <gshelly1402@gmail.com>
2021-07-11 23:34:37 -0700

    indent the code

--- src/components/Person.js
+++ src/components/Person.js
@@ -8,7 +8,9 @@ const Person = (props) => {
             <h1>{lastName}, {firstName}</h1>
             <p>Age: {getAge}</p>
             <p>Hair Color: {hairColor}</p>
-            <button onClick = {(event) => setAge( getAge + 1) }> Birthday Button for {firstName} {lastName}</button>
+            <button onClick = {(event) => setAge( getAge + 1) }> 
+            Birthday Button for {firstName} {lastName}
+            </button>
         </div>
     )
 }
